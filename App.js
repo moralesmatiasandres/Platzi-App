716848e4c8171eb531a353d1fab74dfa70224d37
MAtias Morales <moralesmatiasandres@gmail.com>
2019-07-17 14:57:23 -0300

    Terminada la lista de categorias

--- App.js
+++ App.js
@@ -3,17 +3,22 @@ import { Text } from 'react-native';
 import Home from './src/screens/container/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/container/SuggestionList';
+import CategoryList from './src/videos/container/CategoryList';
 import API from './utils/Api';
 
 class App extends React.Component {
    state = {
      suggestionList: [],
+     categoryList: [],
    }  
    async componentDidMount() {
     const movies = await API.getSuggestion(10);
+    const categories = await API.getMovies();
     console.log(movies)
+    console.log(categories)
     this.setState({
       suggestionList: movies,
+      categoryList: categories,
     })
   }
 
@@ -23,6 +28,9 @@ class App extends React.Component {
         <Header />
         <Text>Buscador</Text>
         <Text>Categorias</Text>
+        <CategoryList 
+          list={this.state.categoryList}
+        />
         <SuggestionList 
           list={this.state.suggestionList}
         />
